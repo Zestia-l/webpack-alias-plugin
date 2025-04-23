8017705e66c28f7c5f219d4399afb198eddbcfdb
Zestia-l <2629774016@qq.com>
2025-04-23 23:05:59 +0800

    feat:基本逻辑编写以及依赖关系初始化

---
+++ packages/main-app/src/index.tsx
@@ -0,0 +1,6 @@
+
+import { greet, Button } from 'sdk';
+import React from 'react';
+
+console.log(greet());
+const App = () => <Button />;
---
+++ packages/sdk/src/components/index.tsx
@@ -0,0 +1,2 @@
+import React from 'react'
+export const Button = () => {return <button>SDK</button>}
