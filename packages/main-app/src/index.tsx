4fb3ce9a9925b0331beab35a1ed823a8fdd8e706
Zestia-l <2629774016@qq.com>
2025-04-24 11:53:33 +0800

    fix:配置文件调整

--- packages/main-app/src/index.tsx
+++ packages/main-app/src/index.tsx
@@ -1,6 +1,4 @@
-
 import { greet, Button } from 'sdk';
-import React from 'react';
 
 console.log(greet());
 const App = () => <Button />;
--- packages/main-app/webpack.config.js
+++ packages/main-app/webpack.config.js
@@ -38,8 +38,6 @@ module.exports = {
     // 路径别名映射
     alias: {
       '@': path.resolve(__dirname, 'src'),       // 映射项目根目录下的 src 文件夹
-      components: path.resolve(__dirname, 'src/components'), // 组件目录
-      utils: path.resolve(__dirname, 'src/utils'),           // 工具函数目录
     },
     // 自动补全文件扩展名
     extensions: ['.js', '.jsx', '.json', '.css','.ts','.tsx']
