0c07f64ca253633b6acbc87695980fd214168483
Zhousitong <sitong.zhou@shopline.com>
2025-05-08 20:36:41 +0800

    feat:tsconfig文件解析主函数逻辑

---
+++ plugins/webpack-alias-plugin/src/loadAllTsconfigs/index.ts
@@ -0,0 +1,30 @@
+import { globSync } from "fs";
+import path from "path";
+import * as fs from 'fs';
+import { parseTsConfig } from "./utils/parseTsConfig";
+import { normalizeAliasPath } from "./utils/normalizeAliasPath";
+import { Aliases } from "./types";
+
+export function loadAllTsConfigs(packagePatterns:string[], rootPath:string, tsConfigName:string) {
+    const aliases:Aliases= {};//所有提取到的别名信息
+    
+    packagePatterns.forEach(pattern => {
+      globSync(pattern, { cwd: rootPath }).forEach(pkgDir => {
+        const tsConfigPath = path.join(rootPath, pkgDir, tsConfigName);
+        if (fs.existsSync(tsConfigPath)) {
+          const AliasesAndPaths = parseTsConfig(tsConfigPath);
+          if(AliasesAndPaths?.baseUrl&&AliasesAndPaths?.paths)
+          {
+            const { baseUrl, paths } = AliasesAndPaths;
+            Object.entries(paths).forEach(([alias, mappings]) => {
+            const normalizedPath = normalizeAliasPath(
+              pkgDir, baseUrl, (Array.isArray(mappings) ? mappings[0] : '')
+            );
+            aliases[alias.replace('/*', '')] = normalizedPath;
+          });
+          }
+        }
+      });
+    });
+    return aliases;
+  }
--- plugins/webpack-alias-plugin/src/loadAllTsconfigs/types/index.ts
+++ plugins/webpack-alias-plugin/src/loadAllTsconfigs/types/index.ts
@@ -1,3 +1,7 @@
 export interface Ref{
     path:string;
 }
+//索引签名，
+export interface Aliases{
+    [alias: string]: string;
+}
